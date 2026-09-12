90b7a2a4c7a31a75b0d92f91cc6903bfbccb75f6
Anirod310 <bousek.dorian@gmail.com>
2026-09-12 20:15:59 +0200

    Update manual_play.py to change render mode to 'rgb_array' and remove unused code; make it playable for Atari game Breakout -v5

--- src/rainbow/evaluation/manual_play.py
+++ src/rainbow/evaluation/manual_play.py
@@ -1,17 +1,13 @@
 import gymnasium as gym
 import ale_py
+from gymnasium.utils.play import play
 
 gym.register_envs(ale_py)
 
-env = gym.make('ALE/Breakout-v5', obs_type="rgb", repeat_action_probability=0.0, render_mode="human")
+env = gym.make('ALE/Breakout-v5', obs_type="rgb", repeat_action_probability=0.0, render_mode="rgb_array")
 
-obs, info = env.reset()
+play(env, keys_to_action={('e',): 0, (' ',): 1, ('d',): 2, ('q',): 3})
 
-done = False
 
-while not done :
-    obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
-    if terminated or truncated :
-        done = True
 
-env.close()
+
