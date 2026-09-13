3c525b3f2ed8ccd1cb35cbe2e0eebfc92bc49790
Anirod310 <bousek.dorian@gmail.com>
2026-09-13 18:32:25 +0200

    Refactor BaseModel to inherit from nn.Module and create CNNBackbone to have the cnn block of the model

--- src/rainbow/models/cnn_model.py
+++ src/rainbow/models/cnn_model.py
@@ -1,8 +1,29 @@
 import torch
-import torch.nn.functional as nn
-from base_model import BaseModel
+import torch.nn as nn
+from .base_model import BaseModel
 
-class CNNAtari(BaseModel):
-    def __init__(self, input_dim, output_dim):
+class CNNBackbone(BaseModel):
+    def __init__(self, input_dim):
         super().__init__()
-    
+        self.model = nn.Sequential(nn.Conv2d(input_dim[0], 32, kernel_size=(8,8), stride=4),
+                              nn.ReLU(),
+                              nn.Conv2d(32, 64, kernel_size=(4,4), stride=2),
+                              nn.ReLU(),
+                              nn.Conv2d(64, 64, kernel_size=(3,3), stride=1),
+                              nn.ReLU(),
+                              nn.Flatten(),
+                              nn.LazyLinear(out_features=512))
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+tensor = torch.rand(1, 4, 84, 84)
+
+model = CNNBackbone(tensor.shape)
+
+output = model(tensor)
+
+print(tensor.shape)
+
+print(output.shape)
