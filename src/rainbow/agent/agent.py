6a667cfa28868793d5d7845db0ffccf2a74c82e4
Anirod310 <bousek.dorian@gmail.com>
2026-09-23 23:06:50 +0200

    Modify the preprocessor output to be in the type we want (ie float32 np array)

--- src/rainbow/agent/agent.py
+++ src/rainbow/agent/agent.py
@@ -1,9 +1,11 @@
-import gymnasium as gym
-
+import torch
 
 class Agent():
     def __init__(self, model, action_space, epsilon):
+        self.model = model
+        self.action_space = action_space
         self.epsilon = epsilon
 
-    def select_action():
-        ...
+    def select_action(self, observation):
+        observation_tensor = torch.tensor(observation).unsqueeze(0)
+
